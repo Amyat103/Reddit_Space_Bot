43ad5c9af97b77c0f104ede1519089a50a30d8b5
Amyat103 <zhoudavid7312@gmail.com>
2023-10-06 16:30:18 -0400

    added submission instance to test reddit connection

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ reddit = praw.Reddit(
     username=secret.username,
 )
 
+#authorized instance instead of read-only
 print(reddit.read_only)
+
+#testing subreddit created for this bot
+#obtaining subreddit
+subreddit = reddit.subreddit("SpaceGalleries")
+print(subreddit.display_name)
+print(subreddit.description)
+
+#obtaining submission instance from a sub
+redditor = reddit.redditor(secret.username)
+print(redditor.link_karma)
+

85eb75a6a52883132f149e8bad51c9b1c36ebcfd
Amyat103 <zhoudavid7312@gmail.com>
2023-10-06 18:21:42 -0400

    took hdurl link from nasa api and posting with reddit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 #PRAW
-def reddit_post():
+def reddit_post(pic):
     #Authenticating via OAuth
     reddit = praw.Reddit(
         client_id=secret.client_id,
@@ -29,28 +29,28 @@ def reddit_post():
     print(redditor.link_karma)
 
     sub_title = "SpaceGalleries"
-    title = "Test"
+    title = "APOD TEST"
     image = r"/Users/david/Desktop/space pic.jpeg"
 
     #Post NASA Image of the day
-    # reddit.subreddit(sub_title).submit_image(title, image)
+    reddit.subreddit(sub_title).submit_image(title, pic)
 
     #Post NASA Monthly Image recap
-    image1 = "/Users/david/Desktop/space pic.jpeg"
-    image2 = "/Users/david/Desktop/Galaxy_clusters_in_the_cosmic_web.jpg"
-    image3 = "/Users/david/Downloads/345984844_770510188014807_6813628122632339994_n.png"
-    images_gal = [
-        {"image_path": image1,
-         "caption": "This is galaxy",
-         },
-        {"image_path": image2,
-         "caption": "This is also galaxy",
-         },
-        {"image_path": image3,
-         "caption": "This is weird tree",
-         },
-    ]
-    reddit.subreddit(sub_title).submit_gallery(title, images_gal)
+    # image1 = "/Users/david/Desktop/space pic.jpeg"
+    # image2 = "/Users/david/Desktop/Galaxy_clusters_in_the_cosmic_web.jpg"
+    # image3 = "/Users/david/Downloads/345984844_770510188014807_6813628122632339994_n.png"
+    # images_gal = [
+    #     {"image_path": image1,
+    #      "caption": "This is galaxy",
+    #      },
+    #     {"image_path": image2,
+    #      "caption": "This is also galaxy",
+    #      },
+    #     {"image_path": image3,
+    #      "caption": "This is weird tree",
+    #      },
+    # ]
+    # reddit.subreddit(sub_title).submit_gallery(title, images_gal)
 
 #NASA API
 #get pic from nasa post on reddit daily
@@ -65,6 +65,10 @@ print(r.status_code)
 print(r)
 response = json.loads(r.text)
 print(response)
+pic = response["hdurl"]
+print(pic)
 
+#use nasa jpg pink to post on subreddit
+reddit_post(pic)
 
 

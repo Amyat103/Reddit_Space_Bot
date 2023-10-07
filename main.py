d53f85f7991a518ec7a66dfd27ebe20920c16c31
Amyat103 <zhoudavid7312@gmail.com>
2023-10-07 17:18:39 -0400

    added apod_ran_req() for getting random images of APOD

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import requests
 import json
 import os
 import datetime
+from PIL import Image
+from io import BytesIO
 
 #PRAW
-def reddit_post(pic):
+def reddit_post_daily(pic):
     #Authenticating via OAuth
     reddit = praw.Reddit(
         client_id=secret.client_id,
@@ -34,12 +36,11 @@ def reddit_post(pic):
 
     sub_title = "SpaceGalleries"
     title = f"[{today}] NASA Astronomy Picture Of the Day"
-    image = r"/Users/david/Desktop/space pic.jpeg"
 
     #Post NASA Image of the day
     reddit.subreddit(sub_title).submit_image(title, image_path=pic)
 
-    #Post NASA Monthly Image recap
+    #Post CATALOG
     # image1 = "/Users/david/Desktop/space pic.jpeg"
     # image2 = "/Users/david/Desktop/Galaxy_clusters_in_the_cosmic_web.jpg"
     # image3 = "/Users/david/Downloads/345984844_770510188014807_6813628122632339994_n.png"
@@ -56,10 +57,14 @@ def reddit_post(pic):
     # ]
     # reddit.subreddit(sub_title).submit_gallery(title, images_gal)
 
+
+
+
+
 #NASA API
 #get pic from nasa post on reddit daily
 
-def apod_request():
+def apod_request_single():
     #APOD REQUEST
     APOD_params = {
         "date": "",
@@ -80,15 +85,42 @@ def apod_request():
         image.write(image_res.content)
     return "APOD.jpg"
 
-#use nasa jpg pink to post on subreddit
-apod_img = apod_request()
-reddit_post(apod_img)
+#APOD Request 3 random
+def apod_ran_req():
+    ran_params = {
+        "count": 3,
+        "api_key": secret.NASA_API,
+    }
+    r = requests.get("https://api.nasa.gov/planetary/apod", params=ran_params)
+    response = json.loads(r.text)
+    ran_images = []
+    for dict in response:
+        if "hdurl" in dict:
+            ran_images.append(dict["hdurl"])
+        else:
+            ran_images.append(dict["url"])
+    return ran_images
 
+ran_images = apod_ran_req()
+print(ran_images)
+
+
+
+
+
+#use nasa response to post on subreddit
+#daily single post
+# apod_img = apod_request_single()
+# reddit_post_daily(apod_img)
 #deleting the image after posting
-os.remove("APOD.jpg")
+# os.remove("APOD.jpg")
+
+#daily random image from NASA
 
 
-# # TO GENERATE CODE
+
+
+# # TO GENERATE CODE (AUTH)
 # reddit = praw.Reddit(
 #         client_id=secret.client_id,
 #         client_secret=secret.client_secret,
@@ -100,3 +132,18 @@ os.remove("APOD.jpg")
 #
 # print(reddit.auth.url(scopes=["identity"], state=secret.STATE, duration="permanent"))
 #
+
+
+# (MONTHLY CATALOG) MAYBE ADD LATER
+# def apod_request_monthly():
+#     month_params = {
+#         "start_date": "2022-10-12",
+#         "end_date": "2022-10-20",
+#         "api_key": secret.NASA_API,
+#     }
+#     r = requests.get("https://api.nasa.gov/planetary/apod", params=month_params)
+#     print(r.status_code)
+#     response = json.loads(r.text)
+#     print(response)
+
+# print(apod_request_monthly())
